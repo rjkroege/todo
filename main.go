4a94c4897679ab3eecf2472766c6075fc3e6b1cf
Robert Kroeger <rjkroege@liqui.org>
2022-01-16 11:57:24 -0700

    Added flag for specifying taskpaper file

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"log"
 	"flag"
+	"log"
 )
+
+var todofile = flag.String("todofile", "Documents/todo.taskpaper", "path to .taskpaper file")
 
 func main() {
 	log.Println("hello")
@@ -12,6 +14,12 @@ func main() {
 
 	flag.Parse()
 
+	tpf, err := getTaskPaperFilePath(*todofile)
+	if err != nil {
+		// TODO(rjk): some way to give up in a way that makes things helpful in Alfred.
+		log.Fatal(err)
+	}
+
 	if flag.NArg() > 0 {
 		log.Println("running with arguments so need to do something different")
 		return
@@ -19,7 +27,8 @@ func main() {
 
 	log.Println("running without arguments so need to just open TaskPaper")
 
-	// TODO(rjk): decide how I want to log for Alfred debugging?
-	
-}
+	runTaskPaper(tpf)
 
+	// TODO(rjk): decide how I want to log for Alfred debugging?
+
+}
